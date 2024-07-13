8a2aa646fdf703aa9d79438cd8a43d7a13961039
FunKodeT <cooteybug@gmail.com>
2024-07-12 21:48:07 -0600

    -.-; Progress;

--- client/src/app/components/static/fetcher.js
+++ client/src/app/components/static/fetcher.js
@@ -1,21 +1,10 @@
-/* function fetcher(url) {
-	return fetch(url, {mode: 'no-cors'}).then((response) => response.json());
-} */
-
 function fetcher(url) {
-	return fetch(url, {mode: 'cors'})
-		.then((resp) => {
-			if (!resp.ok) {
-				throw `Server error: [${resp.status}] [${resp.statusText}] [${resp.url}]`;
-			}
-			return resp.json();
-		})
-		.then((response) => {
-			response.json();
-		})
-		.then((data) => {
-			resolve(data ? JSON.parse(data) : {});
-		});
+	return fetch(url, {mode: 'cors'}).then((resp) => {
+		if (!resp.ok) {
+			throw `Server error: [${resp.status}] [${resp.statusText}] [${resp.url}]`;
+		}
+		return resp.json();
+	});
 }
 
 export default fetcher;
